51c697c747ae96bfb40bace5e8cc25e7f58e20f2
nmeunier <noemeuniercoli@gmail.com>
2026-02-14 18:41:32 +0100

    finis

---
+++ includes/libft.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmeunier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/06 11:26:13 by nmeunier          #+#    #+#             */
+/*   Updated: 2026/02/13 19:03:10 by nmeunier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <string.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_strjoin(char const *s1, char const *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	**ft_split(char const *s, char c);
+void	ft_putstr_fd(char *s, int fd);
+int		ft_strlen(const char *str);
+
+#endif
---
+++ src/path.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   path.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmeunier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/12 14:14:49 by nmeunier          #+#    #+#             */
+/*   Updated: 2026/02/14 00:17:23 by nmeunier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pipex.h"
+
+char	*ft_getenv(char **env)
+{
+	int		i;
+
+	i = -1;
+	while (env[++i])
+	{
+		if (ft_strncmp(env[i], "PATH=", 5) == 0)
+			return (env[i] + 5);
+	}
+	return (NULL);
+}
+
+char	*get_path(char *cmd, char **env)
+{
+	char	**path;
+	char	**tab_cmd;
+	char	*full_path;
+	char	*is_ok;
+	int		i;
+
+	path = ft_split(ft_getenv(env), ':');
+	tab_cmd = ft_split(cmd, ' ');
+	i = -1;
+	while (path[++i])
+	{
+		full_path = ft_strjoin(path[i], "/");
+		is_ok = ft_strjoin(full_path, tab_cmd[0]);
+		free(full_path);
+		if (access(is_ok, F_OK | X_OK) == 0)
+		{
+			free_tab(path);
+			free_tab(tab_cmd);
+			return (is_ok);
+		}
+		free(is_ok);
+	}
+	free_tab(path);
+	free_tab(tab_cmd);
+	return (cmd);
+}
+
+void	free_tab(char **tab)
+{
+	int	i;
+
+	if (!tab)
+		return ;
+	i = 0;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+}
+
+void	exec_cmd(char *cmd, char **env)
+{
+	char	**tab_cmd;
+	char	*path;
+
+	if (!cmd || cmd[0] == '\0')
+		exit(127);
+	tab_cmd = ft_split(cmd, ' ');
+	if (!tab_cmd || !tab_cmd[0] || tab_cmd[0][0] == '\0')
+	{
+		ft_putstr_fd("command not valid\n", 2);
+		free_tab(tab_cmd);
+		exit(127);
+	}
+	path = get_path(tab_cmd[0], env);
+	if (execve(path, tab_cmd, env) == -1)
+	{
+		ft_putstr_fd("command not found\n", 2);
+		free_tab(tab_cmd);
+		exit(127);
+	}
+}
